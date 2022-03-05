4099d0ba09d339eafa390ff1aef3963c98bb0c8a
Thiago Frinhani <frinhani.frinhani@gmail.com>
2022-03-04 21:35:07 -0300

    Criação de pensamento e middleware de autenticação

--- 11_TOUGHTS/controllers/ToughtController.js
+++ 11_TOUGHTS/controllers/ToughtController.js
@@ -1,3 +1,4 @@
+const session = require('express-session')
 const Tought = require('../models/Tought')
 const User = require('../models/User')
 
@@ -7,5 +8,35 @@ module.exports = class ToughtController{
 
         res.render('toughts/home')
 
+    }
+
+    static async dashboard(req,res){
+        res.render('toughts/dashboard')
+    }
+
+    static createToughts(req,res){
+        res.render('toughts/create')
+    }
+
+    static async createToughtsSave(req,res){
+
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userid
+        }
+
+        try {
+            
+            await Tought.create(tought)
+
+            req.flash('message','Pensamento criado com sucesso')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
