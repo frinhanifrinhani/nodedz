89177427d750cdd85726e37e247dd861d2b42c4c
Thiago Frinhani <frinhani.frinhani@gmail.com>
2022-03-05 18:56:20 -0300

    Busca e ordenação dos pensamentos

--- 11_TOUGHTS/controllers/ToughtController.js
+++ 11_TOUGHTS/controllers/ToughtController.js
@@ -2,12 +2,39 @@ const session = require('express-session')
 const { redirect } = require('express/lib/response')
 const Tought = require('../models/Tought')
 const User = require('../models/User')
+const { Op } = require('sequelize')
 
 module.exports = class ToughtController{
 
     static async showTought(req, res){
 
-        res.render('toughts/home')
+        let search = ''
+
+        if(req.query.search){
+            search = req.query.search
+        }
+
+        let order = 'DESC';
+
+        if(req.query.order === 'old'){
+            order = 'ASC'
+        }else{
+            order = 'DESC'
+        }
+
+        const toughtsData = await Tought.findAll({
+            include:User,
+            where: {
+                title: {[Op.like]: `%${search}%`}
+            },
+            order: [['createdAt', order]]
+        })
+
+        const toughts = toughtsData.map((result) => result.get({plain:true}))
+
+        let toughtsQty = toughts.length
+
+        res.render('toughts/home',{toughts, search, toughtsQty})
 
     }
 
