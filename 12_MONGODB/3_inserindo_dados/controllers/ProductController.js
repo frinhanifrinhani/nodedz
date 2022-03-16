c2c561da8de3455fadd914d87a654b73098b3d56
Thiago Frinhani <frinhani.frinhani@gmail.com>
2022-03-16 17:43:28 -0300

    Inserindo produto (front-end e back-end)

---
+++ 12_MONGODB/3_inserindo_dados/controllers/ProductController.js
@@ -0,0 +1,27 @@
+const Product = require('../models/Product')
+
+module.exports = class ProductController{
+
+    static showproducts(req,res) {
+        
+        res.render('products/all')
+    }
+
+    static createProduct(req,res){
+        res.render('products/create')
+    }
+
+    static async createProductPost(req,res){
+
+        const name = req.body.name
+        const price = req.body.price
+        const description = req.body.description 
+
+        const product = new Product(name,price,description)
+
+        product.save()
+        
+        res.redirect('/products')
+
+    }
+}
