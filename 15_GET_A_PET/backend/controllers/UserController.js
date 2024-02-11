0b9b7c24e693084af21831d0cd9c65c70be8d45e
Thiago Frinhani <thiago.frinhani@ths.inf.br>
2024-02-11 16:50:42 -0300

    Validação dados usuário

--- 15_GET_A_PET/backend/controllers/UserController.js
+++ 15_GET_A_PET/backend/controllers/UserController.js
@@ -3,6 +3,50 @@ const User = require('../models/User')
 module.exports = class UserController {
 
     static async register(req, res) {
-        res.json({ message: 'Cadastrado com sucesso' })
+        const { name, email, phone, password, confirmpassword } = req.body
+
+        // validations
+        if (!name) {
+            res.status(422).json({ message: 'O campo nome precisa ser preenchido!' })
+            return
+        }
+
+        if (!email) {
+            res.status(422).json({ message: 'O campo email precisa ser preenchido!' })
+            return
+        }
+
+        if (!phone) {
+            res.status(422).json({ message: 'O campo phone precisa ser preenchido!' })
+            return
+        }
+
+        if (!password) {
+            res.status(422).json({ message: 'O campo password precisa ser preenchido!' })
+            return
+        }
+
+        if (!confirmpassword) {
+            res.status(422).json({ message: 'O campo confirmpassword precisa ser preenchido!' })
+            return
+        }
+
+        if (password !== confirmpassword) {
+            res.status(422).json({ message: 'A senha e a confirmação de senha precisam ser identicos!' })
+            return
+        }
+
+        // check if user exists
+        const userExists = await User.findOne({ email: email })
+
+        if (userExists) {
+            res.status(409).json({ message: 'Este email já está sendo útilizado, tente outro email!' })
+        }
+
+
+        res.status(201).json({ message: 'Cadastrado com sucesso!' })
+
+
+
     }
 }
