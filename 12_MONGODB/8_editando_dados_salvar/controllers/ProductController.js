a208b2854409ff65fd5cd541ac08ba8c83c2ae9b
Thiago Frinhani <thiago.frinhani@ths.inf.br>
2024-02-11 12:15:36 -0300

    Ajustes salvar produtor

--- 12_MONGODB/8_editando_dados_salvar/controllers/ProductController.js
+++ 12_MONGODB/8_editando_dados_salvar/controllers/ProductController.js
@@ -1,34 +1,34 @@
 const Product = require('../models/Product')
 
-module.exports = class ProductController{
+module.exports = class ProductController {
 
-    static async showproducts(req,res) {
+    static async showproducts(req, res) {
 
         const products = await Product.getProducts()
-        
-        res.render('products/all',{ products })
+
+        res.render('products/all', { products })
     }
 
-    static createProduct(req,res){
+    static createProduct(req, res) {
         res.render('products/create')
     }
 
-    static async createProductPost(req,res){
+    static async createProductPost(req, res) {
 
         const name = req.body.name
         const image = req.body.image
         const price = req.body.price
-        const description = req.body.description 
+        const description = req.body.description
 
-        const product = new Product(name,image,price,description)
+        const product = await new Product(name, image, price, description)
 
         product.save()
-        
+
         res.redirect('/products')
 
     }
 
-    static async getProduct(req,res){
+    static async getProduct(req, res) {
 
         const id = req.params.id
         const product = await Product.getProductbyId(id)
@@ -36,8 +36,8 @@ module.exports = class ProductController{
         res.render('products/product', { product })
     }
 
-    static async removeProduct(req,res){
-        
+    static async removeProduct(req, res) {
+
         const id = req.params.id
 
         await Product.removeProductById(id)
@@ -45,17 +45,17 @@ module.exports = class ProductController{
         res.redirect('/products')
     }
 
-    static async editProduct(req,res){
+    static async editProduct(req, res) {
 
         const id = req.params.id
 
         const product = await Product.getProductbyId(id)
 
-        res.render('products/edit',{product})
+        res.render('products/edit', { product })
 
     }
 
-    static async editProductPost(req,res){
+    static async editProductPost(req, res) {
 
         const id = req.body.id
         const name = req.body.name
@@ -63,7 +63,7 @@ module.exports = class ProductController{
         const price = req.body.price
         const description = req.body.description
 
-        const product = new Product(name,image,price,description)
+        const product = new Product(name, image, price, description)
 
         await product.updateProduct(id)
 
--- 12_MONGODB/8_editando_dados_salvar/models/Product.js
+++ 12_MONGODB/8_editando_dados_salvar/models/Product.js
@@ -2,16 +2,16 @@ const conn = require('../db/conn')
 
 const { ObjectId } = require('mongodb')
 
-class Product{
+class Product {
 
-    constructor(name,image,price,description){
+    constructor(name, image, price, description) {
         this.name = name
         this.image = image
         this.price = price
         this.description = description
     }
 
-    save(){
+    save() {
         const product = conn.db().collection('products').insertOne({
             name: this.name,
             image: this.image,
@@ -22,30 +22,31 @@ class Product{
         return product;
     }
 
-    static getProducts(){
+    static getProducts() {
 
         const products = conn.db().collection('products').find().toArray()
 
         return products
     }
 
-    static async getProductbyId(id){
+    static async getProductbyId(id) {
 
-        const product = await conn.db().collection('products').findOne({_id: ObjectId(id)})
+        const product = await conn.db().collection('products').findOne({ _id: ObjectId(id) })
+        // com a versão mais atual ->> ({ _id: ObjectId.createFromHexString(id) })
 
         return product
     }
 
-    static async removeProductById(id){
+    static async removeProductById(id) {
 
-        await conn.db().collection('products').deleteOne({_id: ObjectId(id)})
+        await conn.db().collection('products').deleteOne({ _id: ObjectId(id) })
 
         return
     }
 
-    updateProduct(id){
+    updateProduct(id) {
 
-        conn.db().collection('products').updateOne({_id: ObjectId(id)},{$set: this})
+        conn.db().collection('products').updateOne({ _id: ObjectId(id) }, { $set: this })
     }
 }
 
