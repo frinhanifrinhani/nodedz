12dfcafd8d750dbc5aae635f93b7712dd0f218f5
Thiago Frinhani <thiago.frinhani@ths.inf.br>
2024-02-21 22:59:57 -0300

    Css navbar e footer

--- 15_GET_A_PET/frontend/src/App.js
+++ 15_GET_A_PET/frontend/src/App.js
@@ -2,10 +2,11 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigete,
+  Navigate,
 } from 'react-router-dom'
 
 import Navbar from './components/layout/Navbar'
+import Container from './components/layout/Container'
 import Footer from './components/layout/Footer'
 
 import Home from './components/pages/Home'
@@ -16,11 +17,13 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Container>
       <Footer />
     </Router>
   );
--- 15_GET_A_PET/frontend/src/components/layout/Navbar.js
+++ 15_GET_A_PET/frontend/src/components/layout/Navbar.js
@@ -24,6 +24,8 @@ function Navbar() {
             </ul>
         </nav>
     )
+
+
 }
 
 export default Navbar
