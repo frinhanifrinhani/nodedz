b75f2585c95a9e8fe3efc87583b324684777aeff
Thiago Frinhani <thiago.frinhani@ths.inf.br>
2024-02-22 14:09:57 -0300

    Pagina de registro

--- 15_GET_A_PET/frontend/src/components/pages/Auth/Register.js
+++ 15_GET_A_PET/frontend/src/components/pages/Auth/Register.js
@@ -1,7 +1,58 @@
+import Input from '../../form/Input'
+import { Link } from 'react-router-dom'
+
+import styles from '../../form/Form.module.css'
+
 function Register() {
+    function handleChange(e) {
+
+    }
+
     return (
-        <section>
+        <section className={styles.form_container}>
             <h1>Register</h1>
+            <form >
+                <Input
+                    text='Nome'
+                    type='text'
+                    name='name'
+                    placeholder='Digite seu nome'
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text='Telefone'
+                    type='text'
+                    name='phone'
+                    placeholder='Digite seu telefone'
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text='E-mail'
+                    type='email'
+                    name='email'
+                    placeholder='Digite seu emial'
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text='Senha'
+                    type='password'
+                    name='password'
+                    placeholder='Digite sua senha'
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text='Senha'
+                    type='password'
+                    name='password'
+                    placeholder='Confirmar sua senha'
+                    handleOnChange={handleChange}
+                />
+
+                <input type="submit" value="Cadastrar" />
+            </form>
+            <p>
+                Já tem conta? <Link to='/login'>Clique aqui</Link>
+            </p>
         </section>
     )
 }
