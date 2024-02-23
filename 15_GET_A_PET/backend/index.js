d662d8118d76d79e9e7824e8535fa366a06c72b9
Thiago Frinhani <thiago.frinhani@ths.inf.br>
2024-02-22 23:01:04 -0300

    Ajuste cors

--- 15_GET_A_PET/backend/index.js
+++ 15_GET_A_PET/backend/index.js
@@ -3,11 +3,12 @@ const cors = require('cors')
 
 const app = express()
 
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
 // Config JSON response
 app.use(express.json())
-
-// Solve CORS
-app.use(cors({ credentials: true, origin: 'http:localhost:3000' }))
 
 // Public folder for images
 app.use(express.static('public'))
