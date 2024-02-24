d0ba09bf4aa217dedbcf02a2b4d273f10a7dca7b
Thiago Frinhani <thiago.frinhani@ths.inf.br>
2024-02-23 22:52:00 -0300

    Fomulario de login

--- 15_GET_A_PET/frontend/src/components/pages/Auth/Login.js
+++ 15_GET_A_PET/frontend/src/components/pages/Auth/Login.js
@@ -1,7 +1,38 @@
+import { useState, useContext } from "react"
+
+import Input from '../../form/Input'
+
+import styles from '../../form/Form.module.css'
+
+import { Context } from "../../../context/UserContext"
+
 function Login() {
+
+    function handleChange(e) {
+
+    }
+
     return (
-        <section>
+        <section className={styles.form_container}>
             <h1>Login</h1>
+            <form>
+                <Input
+                    text="E-mail"
+                    type="email"
+                    name="email"
+                    placeholder="Digite o seu email"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Senha"
+                    type="password"
+                    name="password"
+                    placeholder="Digite a sua senha"
+                    handleOnChange={handleChange}
+                />
+
+                <input type="submit" value="Entrar" />
+            </form>
         </section>
     )
 }
