c5576e8128aa1f6e53d3705f9b0bea2811dae686
Thiago Frinhani <thiago.frinhani@ths.inf.br>
2024-02-24 14:32:26 -0300

    Estrutura inicial pag addpet

--- 15_GET_A_PET/frontend/src/App.js
+++ 15_GET_A_PET/frontend/src/App.js
@@ -15,6 +15,7 @@ import Register from './components/pages/Auth/Register'
 import Login from './components/pages/Auth/Login'
 import Profile from './components/pages/User/Profile'
 import MyPets from './components/pages/Pet/MyPets'
+import AddPet from './components/pages/Pet/AddPet'
 
 import { UserProvider } from './context/UserContext'
 
@@ -30,6 +31,7 @@ function App() {
             <Route path='/register' element={<Register />} />
             <Route path='/user/profile' element={<Profile />} />
             <Route path='/pet/mypets' element={<MyPets />} />
+            <Route path='/pet/add' element={<AddPet />} />
             <Route path='/' element={<Home />} />
           </Routes>
         </Container>
