a64717c2c5ce87744015538a969c71a29b363a32
Thiago Frinhani <thiago.frinhani@ths.inf.br>
2024-02-23 23:08:36 -0300

    Login na api

--- 15_GET_A_PET/frontend/src/hooks/useAuth.js
+++ 15_GET_A_PET/frontend/src/hooks/useAuth.js
@@ -48,6 +48,28 @@ export default function useAuth() {
         navigate('/')
     }
 
+    async function login(user) {
+        let msgText = 'Login realizado com sucesso'
+        let msgType = 'success'
+
+        try {
+
+            const data = await api.post('/users/login', user).
+                then((response) => {
+                    return response.data
+                })
+
+            await authUser(data)
+
+        } catch (error) {
+            msgText = error.response.data.message
+            msgType = 'error'
+        }
+
+        setFlashMessage(msgText, msgType)
+
+    }
+
     function logout() {
         const msgText = 'Logout realizado com sucesso'
         const msgType = 'success'
@@ -62,5 +84,5 @@ export default function useAuth() {
 
     }
 
-    return { authenticated, register, logout }
+    return { authenticated, register, login, logout }
 }
